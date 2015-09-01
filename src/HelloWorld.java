2551ed64c4270647ddc5d7b550754405af8a197f
pavan <pavan2k15kumar@gmail.com>
2015-09-01 14:06:29 +0530

    modified helloworld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         Disp d=new Disp();
-        System.out.println("hello");
+        System.out.println("HELLO");
         System.out.println("Another line change");
         d.Display();
         Test t=new Test();
